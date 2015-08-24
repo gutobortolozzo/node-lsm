e6d30d8d3ffca839e99dcbd6444df22af1deb6c0
joao.bortolozzo <joao.bortolozzo@objective.com.br>
2015-08-23 21:10:45 -0300

    Initial tests using mocha

--- test/main.js
+++ test/main.js
@@ -1,31 +1,52 @@
 var GoatDB = require('../index.js');
 
 var db = GoatDB('/tmp/tbd', {
-    threshold : 1
+    threshold : 100
 });
 
-db.open()
-    .then(function(){
-        return db.put("123", "Oi");
-    })
-    .then(function(){
-        return db.get("123");
-    })
-    .then(function(result){
-        console.log(result);
-    })
-    .then(function(){
-        return db.put('1234', 'Tchauuu');
-    })
-    .then(function(){
-        return db.put('12345', 'hehehehe');
-    })
-    .then(function(){
-        return db.put('123456', ':D');
-    })
-    .then(function(){
-        return db.get('12345');
-    })
-    .then(function(result){
-        console.log(result);
-    })
+describe('PUT', function() {
+
+    beforeEach(function(){
+        return db.open();
+    });
+
+    describe('save one value', function() {
+
+        it('should save one value without error', function(){
+            return db.put("123", "Oi");
+        });
+
+        it('should save two values without error', function(){
+            return db.put("123", "Name")
+                .then(function(){
+                    return db.put('1234', 'Value');
+                });
+        });
+    });
+});
+
+//db.open()
+//    .then(function(){
+//        return db.put("123", "Oi");
+//    })
+//    .then(function(){
+//        return db.get("123");
+//    })
+//    .then(function(result){
+//        console.log(result);
+//    })
+//    .then(function(){
+//        return db.put('1234', 'Tchauuu');
+//    })
+//    .then(function(){
+//        return db.put('12345', 'hehehehe');
+//    })
+//    .then(function(){
+//        return db.put('123456', ':D');
+//    })
+//    .then(function(){
+//        return db.get('12345');
+//    })
+//    .then(function(result){
+//        console.log(result);
+//    })
