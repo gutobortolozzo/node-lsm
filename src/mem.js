1b7342ea4acd52809f5007f858866dbb607df140
joao.bortolozzo <joao.bortolozzo@objective.com.br>
2015-08-26 00:50:34 -0300

    Indentation of file

--- src/mem.js
+++ src/mem.js
@@ -3,25 +3,29 @@ var pull = require('pull-stream')
 var next = setImmediate || process.nextTick
 
 module.exports = function () {
-  var store = {}
-  var keys = []
-  var db
-  return db = {
-    size: 0,
-    level: 1,
-    type: 'mem',
-    store: store,
-    get: function (key, cb) {
-      if(!store[key]) return cb(new Error('not found'))
-      next(function () {cb(null, store[key])})
-    },
-    put: function (key, value, cb) {
-      store[key] = value
-      keys.push(key)
-      keys.sort()
-      db.size ++
-      next(function () { cb() })
-    },
+    var store = {}
+    var keys = []
+    var db
+    return db = {
+        size: 0,
+        level: 1,
+        type: 'mem',
+        store: store,
+        get: function (key, cb) {
+            if (!store[key]) return cb(new Error('not found'))
+            next(function () {
+                cb(null, store[key])
+            })
+        },
+        put: function (key, value, cb) {
+            store[key] = value
+            keys.push(key)
+            keys.sort()
+            db.size++
+            next(function () {
+                cb()
+            })
+        },
 //LATER
 //    del: function (key, cb) {
 //      if(!store[key]) return cb(new Error('not found'))
@@ -30,17 +34,17 @@ module.exports = function () {
 //      delete store[key]
 //      cb()
 //    },
-    createReadStream: function (opts) {
-      var gt = opts && opts.gt
-      return pull.values(keys.filter(function (e) {
-        return gt ? e.key > gt : true
-      }).map(function (key) {
-        return (
-          opts && opts.values === false ? key
-        : opts && opts.keys   === false ? store[key]
-        :                                 { key: key, value: store[key] }
-        )
-      }))
+        createReadStream: function (opts) {
+            var gt = opts && opts.gt
+            return pull.values(keys.filter(function (e) {
+                return gt ? e.key > gt : true
+            }).map(function (key) {
+                return (
+                    opts && opts.values === false ? key
+                        : opts && opts.keys === false ? store[key]
+                        : {key: key, value: store[key]}
+                )
+            }))
+        }
     }
-  }
 }
